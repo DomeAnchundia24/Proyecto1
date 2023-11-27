a97b6831ffec316326a789f82768fe0533885a20
DomeAnchundia24 <135488626+DomeAnchundia24@users.noreply.github.com>
2023-11-26 23:55:08 -0500

    primer proyecto

--- sap/policia/admin.py
+++ sap/policia/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from policia.models import policia, rango, especialidad
 
-# Register your models here.
+
 admin.site.register(policia)
 admin.site.register(rango)
 admin.site.register(especialidad)
